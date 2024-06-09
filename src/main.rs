83b08c882a65b8502b561f7789205218d5087c78
vmanikes <vmanikes@hawk.iit.edu>
2024-06-09 17:58:03 -0600

    The type builtin: builtins

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,36 @@ use std::io::{self, Write};
 use std::process::exit;
 
 fn main() {
+    let builtin_commands: Vec<&str> = vec!["exit", "echo"];
+
     let stdin = io::stdin();
-    let mut command = String::new();
+    let mut input = String::new();
 
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    while stdin.read_line(&mut command).is_ok() {
-        if command.contains("exit") {
-            handle_exit(&command);
-        } else if command.contains("echo") {
-            handle_echo(&command);
-            command.clear();
-        } else {
-            println!("{}: command not found", command.trim());
-            command.clear();
+    while stdin.read_line(&mut input).is_ok() {
+        let command = input.split_whitespace().collect::<Vec<&str>>();
+        let parameters = command.clone().into_iter().skip(1).collect::<Vec<&str>>();
+
+        if command.len() >= 1 {
+            match command[0] {
+                "exit" => {
+                    handle_exit(&parameters);
+                },
+                "echo" => {
+                    handle_echo(&parameters);
+                    input.clear();
+                },
+                "type" => {
+                    handle_type(&builtin_commands, &parameters);
+                    input.clear();
+                },
+                _ => {
+                    println!("{}: command not found", command[0].trim());
+                    input.clear();
+                }
+            }
         }
 
         print!("$ ");
@@ -25,21 +40,24 @@ fn main() {
     }
 }
 
-fn handle_echo(command: &String) {
-    let commands: Vec<&str> = command.split_whitespace().collect();
-    let result: Vec<&str> = commands.into_iter().skip(1).collect();
-
-    println!("{}", result.join(" ").trim());
+fn handle_type(builtin_commands: &Vec<&str>, parameters: &Vec<&str>) {
+    if builtin_commands.contains(&parameters[0]) {
+        println!("{} is a shell builtin", parameters[0])
+    } else {
+        println!("{} not found", parameters[0])
+    }
 }
 
-fn handle_exit(command: &String) {
-    let code: Vec<&str> = command.split_whitespace().collect();
+fn handle_echo(parameters: &Vec<&str>) {
+    println!("{}", parameters.join(" ").trim());
+}
 
-    if code.len() == 1 {
+fn handle_exit(parameters: &Vec<&str>) {
+    if parameters.len() == 0 {
         exit(0)
     }
 
-    match code[1].trim().parse::<i32>() {
+    match parameters[0].trim().parse::<i32>() {
         Ok(val) => exit(val),
         _ => {exit(0)}
     }
