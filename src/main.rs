ab3422d7e9a00a1f4cbe02cd68744397737b0d4e
vmanikes <vmanikes@hawk.iit.edu>
2024-06-09 12:48:12 -0600

    Handle echo

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,34 @@ fn main() {
     while stdin.read_line(&mut command).is_ok() {
         if command.contains("exit") {
             handle_exit(&command);
+        } else if command.contains("echo") {
+            handle_echo(&command);
+        } else {
+            println!("{}: command not found", command.trim());
+            command.clear();
         }
 
-        println!("{}: command not found", command.trim());
-
-        command.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
     }
 }
 
-fn handle_exit(command: &String) {
-    let code: Vec<&str> = command.split(" ").collect();
+fn handle_echo(command: &String) {
+    let commands: Vec<&str> = command.split_whitespace().collect();
+    let result: Vec<&str> = commands.into_iter().skip(1).collect();
 
-    exit(code[1].trim().parse::<i32>().unwrap());
+    println!("{}", result.join(" ").trim());
+}
+
+fn handle_exit(command: &String) {
+    let code: Vec<&str> = command.split_whitespace().collect();
+
+    if code.len() == 1 {
+        exit(0)
+    }
+
+    match code[1].trim().parse::<i32>() {
+        Ok(val) => exit(val),
+        _ => {exit(0)}
+    }
 }
