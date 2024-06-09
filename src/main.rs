12a922393ed74f1fc05ff85dac6aeb2d79cc5baa
vmanikes <vmanikes@hawk.iit.edu>
2024-06-09 12:36:46 -0600

    Handle exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     let stdin = io::stdin();
@@ -9,10 +10,20 @@ fn main() {
     io::stdout().flush().unwrap();
 
     while stdin.read_line(&mut command).is_ok() {
+        if command.contains("exit") {
+            handle_exit(&command);
+        }
+
         println!("{}: command not found", command.trim());
 
         command.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
     }
+}
+
+fn handle_exit(command: &String) {
+    let code: Vec<&str> = command.split(" ").collect();
+
+    exit(code[1].trim().parse::<i32>().unwrap());
 }
